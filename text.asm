739546eff431bae9b4a329891934d7264a0c7988
UU6202 <marcus0kong@gmail.com>
2021-12-27 16:40:13 +0800

    20211227 original

---
+++ text.asm
@@ -0,0 +1,44 @@
+; this file instructs ca65 to use the character values in SMB1's font instead of the ones in ASCII
+.charmap        '0', $00
+.charmap        '1', $01
+.charmap        '2', $02
+.charmap        '3', $03
+.charmap        '4', $04
+.charmap        '5', $05
+.charmap        '6', $06
+.charmap        '7', $07
+.charmap        '8', $08
+.charmap        '9', $09
+.charmap        'A', $0A
+.charmap        'B', $0B
+.charmap        'C', $0C
+.charmap        'D', $0D
+.charmap        'E', $0E
+.charmap        'F', $0F
+.charmap        'G', $10
+.charmap        'H', $11
+.charmap        'I', $12
+.charmap        'J', $13
+.charmap        'K', $14
+.charmap        'L', $15
+.charmap        'M', $16
+.charmap        'N', $17
+.charmap        'O', $18
+.charmap        'P', $19
+.charmap        'Q', $1A
+.charmap        'R', $1B
+.charmap        'S', $1C
+.charmap        'T', $1D
+.charmap        'U', $1E
+.charmap        'V', $1F
+.charmap        'W', $20
+.charmap        'X', $21
+.charmap        'Y', $22
+.charmap        'Z', $23
+.charmap	' ', $24
+.charmap	'@', $CF	;Copyright sign
+.charmap	'-', $28
+.charmap	'x', $29
+.charmap	'!', $2B
+.charmap	'$', $2E	;Coin sign
+.charmap	'.', $AF
